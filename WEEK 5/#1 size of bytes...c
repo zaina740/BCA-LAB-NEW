9fc07b0ecfe2caf5abb2d65bbbdc007fc8115eec
zaina740 <zainulaabideen66@gmail.com>
2025-09-22 17:08:02 +0530

    #2 COM AND SIM INT

---
+++ WEEK 5/#1 size of bytes...c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{ printf("size of character %d byte \n",sizeof (char));
+printf("size of long %d byte \n",sizeof (long));
+printf("size of double %d byte \n",sizeof (double));
+printf("size of long double %d byte \n",sizeof (long double));
+printf("size of short %d byte \n",sizeof (short));
+printf("size of float %d byte \n",sizeof (float));
+printf("size of integer %d byte \n",sizeof (int));
+}
